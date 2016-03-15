e34a58313e1883dfcdf51c37549081dab7cda97a
Nate Zhang <natezhang@outlook.com>
2016-03-15 23:43:31 +0900

    solve two-sum

---
+++ python/two_sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        candidates = {}
+        for i in xrange(len(nums)):
+            diff = target - nums[i]
+            if diff in candidates:
+                return list((candidates[diff][0], i))
+            candidates[nums[i]] = (i, diff)
+            
